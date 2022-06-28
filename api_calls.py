3c10a4ede00277a17922a0d0ec8a579e650b6331
Blockboy007 <70123625+blockboy007@users.noreply.github.com>
2022-06-27 20:28:59 -0700

    Add files via upload

--- api_calls.py
+++ api_calls.py
@@ -1,24 +1,17 @@
-from selenium import webdriver
 import requests
 import lxml
 import bs4
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+import json
 
 
-def price_api(policy_id='ffa56051fda3d106a96f09c3d209d4bf24a117406fb813fb8b4548e3'):
+def price_api(policy_id):
 
     response_API = requests.get(
         f"https://api.opencnft.io/1/policy/{policy_id}/floor_price")
-    print(response_API.status_code)
     data = response_API.text
     parse_json = json.loads(data)
     floor_price = int(parse_json['floor_price'])/1000000
-    print(f'floor price is: {floor_price}')
+    return floor_price
 
 
 def ada_price_api():
@@ -30,15 +23,8 @@ def ada_price_api():
     return(ada_price)
 
 
-wallet_id = input("Input your wallet address here to get your NFTs.")
-
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-driver.get("https://pool.pm/search/")
-
-element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "search"))
-        )
-search = driver.find_element(By.ID, "search")
-search.send_keys(str(wallet_id))
-search.send_keys(Keys.RETURN)
+################################
+# Example grabbing floor price using policy id, iterating through nft_dict
+################################
+# for key,val in nft_dict.items():
+#     price_api(val[2])
--- nft_policy_router.py
+++ nft_policy_router.py
@@ -1,3 +1,4 @@
+from scrapy.http import TextResponse
 from selenium import webdriver
 import requests
 import lxml
@@ -8,38 +9,96 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from api_calls import ada_price_api
+from api_calls import price_api
+
 
 #########################################
-## Set up for selenium and requests calls
+# Set up for selenium and requests calls
 #########################################
 
 
-wallet_id = "addr1q8vnwuz7tcn5eycs7073m3runv4r2gc7gwd6nkvtjxqtcd7g6vfkkxad5y2mml0e7tw07c6klme5md8mw5cm85lyhuwqyapkxj"
-cardano_scan = f"https://cardanoscan.io/address/{wallet_id}"
-r = requests.get(cardano_scan).content
-# path = "C:\Program Files (x86)\chromedriver.exe"
-# driver = webdriver.Chrome(path)
-# driver.get(cardano_scan)
-soup = bs4.BeautifulSoup(r, "lxml")
+wallet_id = "addr1q8s3nu0awkp6twyd5xszagqvqc7vzpfer8cuxmx46spapjlx4vzk560guuw4tyvhuwpeknkc0l25jp2hxckhvp5g0ygs7j59qw"
 
-#############################################################################################################
-## Getting all information of NFTs in wallet into a dictionary, sorted by asset id[0] and policy id[1] for num
-##############################################################################################################
-nft_dict = {0: ['asset_id', 'policy_id']}
-nft = soup.find_all("option")[10:]
-##### Need to start at 10, because the :9 are built into the cardanoscan website and not relevant
-count = 1
-for item in nft:
+nft_dict = {}
 
-    asset_id = item['value']
-    var = item.text
-    policy_id = var.split(":")[2]
-    nft_dict[count] = [asset_id, policy_id]
-    count += 1
+
+def create_dict(wallet_id):
+
+    nft = soup.find_all("option")[10:]
+    ##### Need to start at 10, because the :9 are built into the cardanoscan website and not relevant
+    count = 1
+    for item in nft:
+
+        asset_id = item['value']
+        var = item.text
+        policy_id = var.split(":")[2][:56]
+        asset_name = var.split(":")[0]
+        nft_dict[count] = [asset_name, asset_id, policy_id]
+        count += 1
+
+    # for key, val in nft_dict.items():
+    #     print(f"{key} --- Asset Name: {val[0]} --- Asset ID: {val[1]} --- Policy ID: {val[2]}")
+
+    return nft_dict
+
+
+def get_img_id(create_dict):
+    #########################
+    # Getting NFT image
+    #########################
+
+    url = 'https://www.coingecko.com/en/coins/cardano'
+    resp = requests.get(url)
+    resp = TextResponse(body=resp.content, url=url)
+
+    resp.css('img.tw-rounded-full').attrib['src']
+
+    ##################################################
+    # getting the real asset #
+    #######################################
+
+    for key, val in nft_dict.items():
+        cs_url = f"https://cardanoscan.io/token/{nft_dict[key][1]}"
+        try:
+            get_assetid = requests.get(cs_url)
+            get_assetid = TextResponse(body=get_assetid.content, url=url)
+            var1 = str(get_assetid.css('div#fingerprintButton').attrib['onclick']
+                       .replace('copyToClipboard', '')
+                       .replace("(", "")
+                       .replace(")", "")
+                       .replace("fingerprintButton", "")
+
+                       )
+
+            new_id = (var1.split(",")[0])[1:-1]
+            print(new_id)
+
+            ### open cexplorer.io with real asset id
+            ceexplorer = f"https://cexplorer.io/asset/{new_id}"
+
+            ### grab img address
+            get_img = requests.get(ceexplorer)
+            get_img = TextResponse(body=get_img.content, url=url)
+
+            img = get_img.css('img.img-fluid').attrib['src']
+            print(img)
+        except:
+            print('This is a native token, not an NFT')
+
+#############################################################
+# Get the ADA and USD price from each policy_id
+#############################################################
+
+
+create_dict(wallet_id)
+get_img_id(create_dict(wallet_id))
+
 
 for key, val in nft_dict.items():
-    print(f"{key} --- Asset ID: {val[0]} --- Policy ID: {val[1]}")
-    
-    
-###########################################
-## Does not grab all NFTs within a wallet if it is in daedalus, multiple addresses are present, need to run through each address to grab all NFTs
+    floor_price = price_api(val[2])
+    usd_price = (floor_price*(ada_price_api()))
+    nft_dict[key].append(floor_price)
+    nft_dict[key].append(usd_price)
+
+print(nft_dict)
